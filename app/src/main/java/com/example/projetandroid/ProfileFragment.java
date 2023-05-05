f6b6d39d61be83d12a50494c85898873eec702c9
bairemkh <bairemkh@gmail.com>
2023-05-05 23:13:01 +0100

    Addtransaction

--- app/src/main/java/com/example/projetandroid/ProfileFragment.java
+++ app/src/main/java/com/example/projetandroid/ProfileFragment.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentTransaction;
 
 public class ProfileFragment extends Fragment {
-
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button closeSettingsBtn = view.findViewById(R.id.close_settings_btn);
+
+        closeSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, new AdoptionFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
