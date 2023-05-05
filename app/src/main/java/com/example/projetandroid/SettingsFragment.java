28ed6e3e72b4918b1b016c3f66c9fbedde890a2e
bairemkh <bairemkh@gmail.com>
2023-05-05 01:47:50 +0100

    AddSettings

--- app/src/main/java/com/example/projetandroid/SettingsFragment.java
+++ app/src/main/java/com/example/projetandroid/SettingsFragment.java
@@ -1,20 +1,65 @@
-package com.example.projetandroid;
 
+package com.example.projetandroid;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class SettingsFragment extends Fragment {
 
+    private TextView battery;
+    private TextView internetConnection;
+    private BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            battery.setText(level + "%");
+        }
+    };
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // Check network connectivity
+        ConnectivityManager cm = (ConnectivityManager) requireActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        internetConnection = view.findViewById(R.id.internetconnection);
+        if (isConnected) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                internetConnection.setText("Device is connected to WiFi");
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                internetConnection.setText("Device is connected to mobile network");
+            }
+        } else {
+            internetConnection.setText("Device is offline");
+        }
+
+        battery = view.findViewById(R.id.batteryLevel);
+        getActivity().registerReceiver(batteryLevelReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        return view;
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().unregisterReceiver(batteryLevelReceiver);
+    }
+
 }
